ead4a77198a817e5570a6055cc6d1a99da985ba4
Maxym <mzaharkiv77@gmail.com>
2023-04-20 19:11:56 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+# u = 'http://chi.download.datapacket.com/100mb.bin'
+# ip = "5.58.55.183"
+# print(requests.get(f'http://api.ipapi.com/api/{ip}?access_key={"65df7c82ddb887fad4c61c3fb1459039"}').json())
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', name="Vitalik")
+
+
+if __name__ == '__main__':
+    app.run()
